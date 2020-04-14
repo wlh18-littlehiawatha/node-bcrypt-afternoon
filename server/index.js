818f216739757ea400021ca9b7e0f86d828ab025
Jacob Hall <halljaco@yahoo.com>
2020-04-14 16:54:00 -0600

    initial commit basic setup installed

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+require('dotenv').config();
+
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
+
+
+const app = express();
+
+
+const PORT = 4000
+
+//top level middleware
+app.use(express.json());
+app.use(session({
+   resave: false,
+   saveUninitialized: true,
+   // cookie: {maxAge 1000 * 60 * 60 * 24 * 7},
+   secret: SESSION_SECRET
+}))
+
+
+// DB connection
+
+massive ({
+   connectionString: CONNECTION_STRING,
+   ssl: {
+      rejectUnauthorized: false
+   }
+}).then(dbInstance => {
+   app.set('db', dbInstance)
+   console.log('DB Connected!')
+   app.listen(PORT, console.log(`Server running on port ${PORT}! Authenticate?`))
+}).catch((error) => console.log(error, `Error with Massive connection`) )
+
+
+
+
+
+
